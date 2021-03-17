6e8a047c0ec9e88c961629d6dc5892985de423b9
ryung <johyunryung@naver.com>
2021-03-17 12:28:06 +0900

    polls/views details, results, vote

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse("hello, world. you're at the polls index")
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at questing %s." % question_id)
+
+def results(request, queston_id):
+    response = "You're looking at the results of question %s"
+    return HttpResponse(response % queston_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
+
